78e1d86b92fc3a7789e46b131a37be5eeac2df28
Elena Savchuk <sana22_soi@student.ztu.edu.ua>
2022-11-12 19:12:26 +0200

    second changed

--- SanaPrakt02/Program.cs
+++ SanaPrakt02/Program.cs
@@ -7,6 +7,8 @@ namespace SanaPrakt02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
